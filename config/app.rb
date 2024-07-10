92bb1dd51fb75e64fd14c014c1bdd2eefc005198
Bogdan <bohdan.parshencev@p2h.com>
2024-07-11 00:12:12 +0300

    init roda

--- config/app.rb
+++ config/app.rb
@@ -2,3 +2,17 @@
 
 require "roda"
 require "slack-ruby-client"
+
+unless defined?(Unreloader)
+  require "rack/unreloader"
+  Unreloader = Rack::Unreloader.new(reload: false)
+end
+
+
+# API entry point
+class SlackManagement < Roda
+  plugin :json
+  plugin :json_parser
+  plugin :all_verbs
+  plugin :symbol_status
+end
